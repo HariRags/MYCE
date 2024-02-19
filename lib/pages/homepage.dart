c220cd433bde5ee04fbd8411233e587eb94ff5df
HariRags <harigovindraghunath@gmail.com>
2024-02-20 03:21:01 +0530

    made homepage responsive

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import '../utilities/responsive.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,24 +13,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     const Color customPurple = Color(0xFF6C3CA9);
     const Color customGrey = Color(0xFF515151);
+    var  leftSpacing= Responsive.width(5.3, context);
     return   Material(
       child: SafeArea(
         child: Scaffold(
           body: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.fromLTRB(21,21,21,0),
+                padding:  EdgeInsets.fromLTRB(leftSpacing,Responsive.height(1.8, context),Responsive.width(5.4, context),0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     InkWell(child: Image.asset('assets/images/burger_icon.png'), onTap: (){},),
-                    Text('Login/Sign up', style: TextStyle(color:customPurple, fontSize:  Responsive.height(2.1, context), fontWeight: FontWeight.w500),)
+                    TextButton(onPressed: (){},child: Text('Login/Sign up', style: TextStyle(color:customPurple, fontSize:  Responsive.height(2, context), fontWeight: FontWeight.w500)),)
                   ],
       
                 ),
               ),
               Container(
-                padding: EdgeInsets.fromLTRB(23, 15, 21, 0),
+                padding: EdgeInsets.fromLTRB(Responsive.width(5.4, context), Responsive.height(1.7, context), Responsive.width(5.7, context), 0),
                 alignment: Alignment.topLeft,
                 child: RichText(
                   text: TextSpan(
@@ -46,34 +45,32 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               Container(
-                padding: EdgeInsets.fromLTRB(23, 5, 50, 0),
-                child: Text('All-in-one civil services app that streamlines real estate, design, progect management, consultancy and flawless execution', softWrap: true, style: TextStyle(color: customGrey, fontWeight: FontWeight.w400),),
+                padding: EdgeInsets.fromLTRB(leftSpacing, Responsive.height(1, context), Responsive.width(10, context), 0),
+                child: const Text('All-in-one civil services app that streamlines real estate, design, project management, consultancy and flawless execution', softWrap: true, style: TextStyle(color: customGrey, fontWeight: FontWeight.w500),),
                 ),
                 SizedBox(
                   height: Responsive.height(10, context),
                 ),
-                 const Expanded(
+                   Expanded(
                    child: Stack(
                     alignment: Alignment.topCenter,
                     children:[
-                     Image(image: AssetImage('assets/images/homepage_folders/Execution.png'),),
+                     const Image(image: AssetImage('assets/images/homepage_folders/Execution.png'),),
                      Positioned(
-                      top:100 ,
-                      child: Image(image: AssetImage('assets/images/homepage_folders/ProjectManagement.png'),)),
+                      top:Responsive.height(11, context) ,
+                      child: const Image(image: AssetImage('assets/images/homepage_folders/ProjectManagement.png'),)),
                       Positioned(
-                      top:180 ,
-                      child: Image(image: AssetImage('assets/images/homepage_folders/DesignAndArchitecture.png'),)),
+                      top:Responsive.height(22, context) ,
+                      child: const Image(image: AssetImage('assets/images/homepage_folders/DesignAndArchitecture.png'),)),
                       Positioned(
-                      top:280 ,
-                      child: Image(image: AssetImage('assets/images/homepage_folders/RealEstate.png'),)),
+                      top:Responsive.height(33, context) ,
+                      child: const Image(image: AssetImage('assets/images/homepage_folders/RealEstate.png'),)),
                       Positioned(
-                      top:350 ,
-                      child: Image(image: AssetImage('assets/images/homepage_folders/SwimmingPool.png'),))
+                      top:Responsive.height(44, context) ,
+                      child: const Image(image: AssetImage('assets/images/homepage_folders/SwimmingPool.png'),))
 
                      ]),
                  )
-                
-              
             ],
           )
         ),
