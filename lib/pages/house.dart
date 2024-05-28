8f6df386cacda93883d6dcf6d1f7d4cd5b669cca
HariRags <harigovindraghunath@gmail.com>
2024-05-29 00:40:19 +0530

    house page widgets made

--- lib/pages/house.dart
+++ lib/pages/house.dart
@@ -43,14 +43,14 @@ class  HouseState extends State <House> {
               ),
             ),
             Container(
-              margin: EdgeInsets.only(left:Responsive.width(3.5, context), bottom: Responsive.height(1.5, context),),
+              margin: EdgeInsets.only(left:Responsive.width(3.5, context), bottom: Responsive.height(3.2, context),),
               child: Row(
                 children: [
-                  Text('Execution', style: TextStyle(fontFamily:'Fraunces', fontSize: Responsive.height(2.1, context), color: Colors.grey),),
+                  Text('Execution', style: TextStyle(fontFamily:'Poppins', fontSize: Responsive.height(2.5, context), color: const Color.fromRGBO(132, 132, 132, 1),letterSpacing: -0.2),),
                   SizedBox(width: Responsive.width(4, context),),
-                  Icon(Icons.arrow_forward_ios_rounded, color: Colors.grey, size: Responsive.height(1.7, context),),
+                  Icon(Icons.arrow_forward_ios_rounded, color: const Color.fromRGBO(132, 132, 132, 1), size: Responsive.height(1.7, context),),
                   SizedBox(width: Responsive.width(4, context),),
-                  Text('House', style: TextStyle(fontFamily:'Fraunces', fontSize: Responsive.height(3, context), color: Colors.black),),
+                  Text('House', style: TextStyle(fontFamily:'Poppins', fontSize: Responsive.height(2.5, context), color:const Color.fromRGBO(70, 70, 70, 1), fontWeight: FontWeight.w600),),
                 ],
               ) ,
             ),
@@ -61,20 +61,48 @@ class  HouseState extends State <House> {
                     InkWell(
                       onTap:(){},
                       child: Card(
+                        margin: EdgeInsets.only(left: Responsive.width(4, context), right: Responsive.width(4, context), bottom: Responsive.height(2, context)),
                         elevation: 6,
                         surfaceTintColor: Colors.white,
                         color: Colors.white,
-                        child: Container(
-                          padding: EdgeInsets.fromLTRB(Responsive.width(7,context), Responsive.height(2, context), Responsive.width(5, context), Responsive.height(2, context)),
-                          height: Responsive.height(13, context),
-                          width: Responsive.width(90, context),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                            Text('Villa', style: TextStyle(fontFamily:'Poppins', fontSize: Responsive.height(2.5, context), color: Colors.black, fontWeight: FontWeight.w500, letterSpacing: 0.5),),
-                            SizedBox(height: Responsive.height(0.4, context),),
-                            Text('A large and luxurious house, often located in a scenic or desirable area.', style: TextStyle(fontFamily: 'Poppins', color: Color.fromRGBO(81, 81, 81, 1)),)
-                          ],),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              
+                              margin: EdgeInsets.only(bottom:Responsive.height(1, context),),
+                              width: Responsive.width(65, context),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                        padding: EdgeInsets.fromLTRB(
+                                          Responsive.width(8, context),
+                                          Responsive.height(2.5, context),
+                                          Responsive.width(5, context),
+                                          0,
+                                        ),
+                               child:  Text('Villa', style: TextStyle(fontFamily:'Poppins', fontSize: Responsive.height(2.5, context), color: Colors.black, fontWeight: FontWeight.w600, letterSpacing: 0.5),),
+                                  ),
+                                Padding(
+                                  padding:  EdgeInsets.fromLTRB(
+                                          Responsive.width(8, context),
+                                          0,
+                                          Responsive.width(5, context),
+                                          Responsive.height(0.6, context),
+                                        ),
+                                  child: Text('A large and luxurious house, often located in a scenic or desirable area.', style: TextStyle(fontFamily: 'Poppins', color: Color.fromRGBO(81, 81, 81, 1)),),
+                                )
+                              ],),
+                            ),
+                           Container(
+                            height: Responsive.height(12.5, context),
+                             child: Image.asset(
+                               'assets/images/villa_pic.png',
+                               fit: BoxFit.cover,
+                             ),
+                           ),
+                          ],
                         ),
                       ),
                     ),
